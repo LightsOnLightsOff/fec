0356cd7ac10284477046c7b4c909e61ec5b3380a
Louisaflor <louisa.yonzon6@gmail.com>
2022-07-08 10:32:57 -0700

    changed webpack and added folders for widgets

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios'
 
 function App (props) {
 
   return (
     <div>
-      <h1>Hello World!!!</h1>
+      <h1>Hello World</h1>
 
     </div>
 
