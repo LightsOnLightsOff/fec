47a532ca0a56e6045b5188dda14d933c646111b4
xiaxia330 <511219774@qq.com>
2022-07-07 20:08:44 -0700

    API calls success

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
 
 function App (props) {
+
+  axios
+    .get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products', {
+      params: { page: 2, count: 2 },
+      headers: {
+        Authorization: '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'
+      }
+    })
+    .then(res => {
+      console.log('this is the response data', res)
+    })
 
   return (
     <div>
       <h1>Hello World!!!</h1>
-
     </div>
-
   )
 }
 
-export default App;
+export default App
 
-ReactDOM.render(<App/>, document.getElementById('root')  )
+ReactDOM.render(<App />, document.getElementById('root'))
---
+++ server/index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+
+const app = express();
+//middleware
+app.use(express.json());
+app.use(express.static('client/dist'));
+
+
+
+
+app.listen(3000,()=>{
+  console.log('listening on port 3000');
+})
+
+
