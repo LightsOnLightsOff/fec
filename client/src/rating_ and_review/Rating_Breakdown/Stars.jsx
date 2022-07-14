d28ddc5c9a6ffb1fc5e28197bfbd81aa42c60cc8
Louisaflor <louisa.yonzon6@gmail.com>
2022-07-14 09:27:17 -0700

    was able to create the 1-5 star breakdown icons

--- client/src/rating_ and_review/Rating_Breakdown/Stars.jsx
+++ client/src/rating_ and_review/Rating_Breakdown/Stars.jsx
@@ -29,14 +29,37 @@ function Stars({ rate, recommended }) {
   recommendKeys.forEach((key, index) => {
     recommendTotal += Number(key)
   })
-console.log("recommendTotal: ", recommendTotal)
+  console.log("recommendTotal: ", recommendTotal)
+
+
 
   return (
     <div className="stars">
-      <h1>{avgTotal} <span  className="starBaby">&#9733;&#9733;&#9733;&#9733;&#9733;</span></h1>
+      <h1>{avgTotal} <span className="starBaby">&#9733;&#9733;&#9733;&#9733;&#9733;</span></h1>
 
       {/* style={{ width: '70%' }} */}
       <h4>{Math.round((recommended['true'] / recommendTotal) * 100)}% of reviews recommend this product</h4>
+
+      <StarBreakdown>
+        <NumStar>5 stars</NumStar>
+        <Breakdown> </Breakdown>
+      </StarBreakdown>
+      <StarBreakdown>
+        <NumStar>4 stars</NumStar>
+        <Breakdown> </Breakdown>
+      </StarBreakdown>
+      <StarBreakdown>
+        <NumStar>3 stars</NumStar>
+        <Breakdown> </Breakdown>
+      </StarBreakdown>
+      <StarBreakdown>
+        <NumStar>2 stars</NumStar>
+        <Breakdown> </Breakdown>
+      </StarBreakdown>
+      <StarBreakdown>
+        <NumStar>1 stars</NumStar>
+        <Breakdown> </Breakdown>
+      </StarBreakdown>
     </div>
   )
 }
@@ -47,3 +70,29 @@ export default Stars;
 const StarBaby = styled.span`
 
 `;
+
+const StarBreakdown = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding-bottom: 10px
+
+
+`;
+
+const Breakdown = styled.span`
+  width: 150px;
+  height: 8px;
+  border-width: thin;
+  margin-top: 4px;
+  margin-left: 10px;
+  background-color: #ddd
+
+`;
+
+const NumStar = styled.p`
+margin: 0px;
+padding: 0px
+padding-right: 5px;
+text-decoration: underline;
+
+`
