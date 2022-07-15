2312ce5e9b5f3ac71144184d66d1f2a79c4a02c2
Siope Tongi <noattongi@gmail.com>
2022-07-15 11:53:44 -0700

    Revert "Shanshan"

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
-import Related from './components/Related.jsx';
 
-function App(){
-  return <Related/>
+
+function App (props) {
+
+  return (
+   <div> Hello </div>
+  )
 }
-// After
-import { createRoot } from 'react-dom/client';
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App  />);
 
-// ReactDOM.render(<Related />, document.getElementById('root'))
+export default App
+
+ReactDOM.render(<App />, document.getElementById('root'))
