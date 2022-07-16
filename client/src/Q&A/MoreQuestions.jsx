00908b1855ee80d9f54aa762661226cb04050b7d
noattongi <noattongi@gmail.com>
2022-07-15 20:14:11 -0700

    Fix MoreQuestions and remove console.logs

--- client/src/Q&A/MoreQuestions.jsx
+++ client/src/Q&A/MoreQuestions.jsx
@@ -1,29 +1,27 @@
 import React, {useState, useEffect} from 'react';
 
+var questionCounter = 2;
+
 function MoreQuestions(props) {
 
   const [showing, changeShowing] = useState(true);
 
-
-
-  var handleClick = (e) => {
-    (async () => {
-      await props.changeCurrentQuestions(props.questionData.results.slice(0, props.currentQuestions.length + 2));
-
-    })()
-    .then(() => {
-      if (props.currentQuestions.length === props.questionData.results.length) {
-
-        changeShowing(false);
-      }
-    })
+  var handleClick = async(e) => {
+    props.changeCurrentQuestions(props.questionData.results.slice(0, props.currentQuestions.length + 2))
   }
+
+  useEffect(() => {
+    if (props.currentQuestions?.length >= props.questionData.results?.length) {
+      changeShowing(false);
+    }
+    else {
+      changeShowing(true);
+    }
+  }, [props.currentQuestions])
 
   if (showing) {
     return (
-      <button onClick={handleClick}>
-        MORE QUESTIONS
-      </button>
+      <button onClick={handleClick}>MORE QUESTIONS</button>
     )
   }
 }
