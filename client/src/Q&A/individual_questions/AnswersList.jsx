1f0454f013e2d0acf870b9323ae8544c8b2a1866
noattongi <noattongi@gmail.com>
2022-07-16 12:10:32 -0700

    added a 'show more answers' button

--- client/src/Q&A/individual_questions/AnswersList.jsx
+++ client/src/Q&A/individual_questions/AnswersList.jsx
@@ -2,18 +2,39 @@ import React, {useState, useEffect} from 'react';
 import AnswersEntry from './AnswersEntry.jsx';
 
 function AnswersList(props) {
+  const [answerData, changeAnswerData] = useState(() => {
+    var answerArray = [];
+    for (let id in props.answers) {
+      answerArray.push(props.answers[id])
+    }
+    return answerArray.sort((a,b) => { return b.helpfulness - a.helpfulness})
+  })
+  const [currentAnswers, changeCurrentAnswers] = useState([]);
+  const [numAnswers, changeNumAnswers] = useState(2);
+  const [showingAll, changeShowingAll] = useState(false)
+
+  useEffect(() => {
+    changeCurrentAnswers(answerData?.slice(0, numAnswers))
+  }, [answerData, numAnswers])
+
+  var handleButton = (e) => {
+    changeNumAnswers(answerData.length)
+    changeShowingAll(true)
+  }
+
   return (
     <div className='answers-list'>
       <h3>
         A:
       </h3>
       <ul>
-        {Object.keys(props.answers).map((answer_id) => {
+        {currentAnswers.map((answer) => {
           return (
-            <AnswersEntry key={answer_id} answer={props.answers[answer_id]}/>
+            <AnswersEntry key={answer.id} answer={answer}/>
           )
         })}
       </ul>
+      <button onClick={handleButton}>show more answers</button>
     </div>
   )
 }
