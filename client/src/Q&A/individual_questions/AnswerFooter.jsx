8a2d710a17b8a46d70b000e8cbdcec64c172a5df
noattongi <noattongi@gmail.com>
2022-07-19 17:49:15 -0700

    added helpful button functionality for answers

--- client/src/Q&A/individual_questions/AnswerFooter.jsx
+++ client/src/Q&A/individual_questions/AnswerFooter.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
+import config from '../../../../config.js';
 import moment from 'moment';
 
 function AnswerFooter(props) {
+
+  const [helpful, setHelpful] = useState(0);
 
   const footerStyle = {
     display: 'flex',
@@ -9,13 +13,26 @@ function AnswerFooter(props) {
     justifyContent: 'space-around'
   }
 
+  console.log('answer: ', props.answer)
 
+  var helpfulClick = () => {
+    axios.put(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/qa/answers/${props.answer.id}/helpful`, null, {
+      headers: {Authorization: config.TOKEN}
+    })
+    .then((response) => {
+      console.log(response);
+      setHelpful(1);
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+  }
 
   return (
     <div className='answer-footer'>
       <p>{props.answer.answerer_name}</p>
       <p>{moment(props.answer.date).fromNow()}</p>
-      <p>helpful?({props.answer.helpfulness})</p>
+      <p>helpful? <a onClick={helpfulClick} className='helpful-yes-button'>Yes</a>({props.answer.helpfulness + helpful})</p>
       <button>report</button>
     </div>
   )
