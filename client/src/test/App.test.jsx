2002ed9f1bb84d0d4f99a35f9b41267c6f55bcfa
Louisaflor <louisa.yonzon6@gmail.com>
2022-07-19 20:55:25 -0700

    now was able to get jest working, still unsure how to use it... does not really work with our certain files, but this is the boiler point for jest, also inform them about babel.config.js

--- client/src/test/App.test.jsx
+++ client/src/test/App.test.jsx
@@ -1,11 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import App from '../index.jsx';
+import Test from '../Test.jsx';
+// import App from '../index.jsx';
+
+
+
+// describe('Addition', () => {
+//   it('knows that 2 and 2 make 4', () => {
+//     expect(2 + 2).toBe(4);
+//   });
+// });
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+// });
+
 
 describe('Search Bar tests', () => {
     it('should contains the heading 1', () => {
-    render(<App />);
-        const heading = screen.getByText(/Hello world! I am using React/i);
+    render(<Test />);
+        const heading = screen.getByText("Hello world! I am using React");
         expect(heading).toBeInTheDocument()
     });
 });
