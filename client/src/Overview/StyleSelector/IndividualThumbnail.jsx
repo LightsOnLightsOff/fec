271e0084ca492a25d3457c9002869e03ded3fc2c
Brandon Hsu <brandon.j.hsu@gmail.com>
2022-07-20 11:09:04 -0700

    changed colors of border when thumbnail is clicked

--- client/src/Overview/StyleSelector/IndividualThumbnail.jsx
+++ client/src/Overview/StyleSelector/IndividualThumbnail.jsx
@@ -24,7 +24,7 @@ function IndividualThumbnail ({setSelectedStyle, style, setClickedName, setOrigi
 
   return (
     <span >
-      <img onClick = {() => {handleClick (event, index)}} style = {{height: 75, width: 'auto', borderRadius: 10, border: index === thumbnailIndex ? '5px solid #4E6E58' : 'none', cursor: 'pointer'}} src={thumbnailURL}/>
+      <img onClick = {() => {handleClick (event, index)}} style = {{height: 75, width: 'auto', borderRadius: 10, border: index === thumbnailIndex ? '5px solid #A6ECE0' : 'none', cursor: 'pointer'}} src={thumbnailURL}/>
     </span>
   )
 }
