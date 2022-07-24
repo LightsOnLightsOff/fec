27f98560ef588e2e7b9651a790d5816fc5ceeb05
xiaxia330 <511219774@qq.com>
2022-07-24 11:23:06 -0700

    fix the star!!!!

--- client/src/Relateditems/SliderImgButton.jsx
+++ client/src/Relateditems/SliderImgButton.jsx
@@ -6,38 +6,47 @@ import 'tiny-slider/dist/tiny-slider.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 
-function ImgButton ({ item, style, index, imgs, clickProduct, clickStar,product }) {
-  const imgStyles = {
-    width: '100%',
-    height: '320px',
-    objectFit: 'cover',
-    position: 'relative'
-  }
-
+function ImgButton ({
+  item,
+  style,
+  index,
+  imgs,
+  clickProduct,
+  clickStar,
+  product
+}) {
   const [show, setShow] = useState(false)
 
   const starStyle = {
-    position: 'absolute',
+    position: 'relative',
     zIndex: '1',
-    color: 'orange'
+    color: 'orange',
+    position: 'fixed',
+    marginLeft:'400px'
   }
 
+  const imgStyles = {
+    width: '100%',
+    height: '320px',
+    objectFit: 'cover'
+  }
 
-// console.log('product in imgandbutton',(product.length-index-1)*350+5)
-    // console.log('let me know the index after render', index)
-    return (
-      <div>
-        <div
-          className='fav'
-          onClick={() => {
-            clickStar(item)
-          }}
-          name={item.name}
-          style={{ ...starStyle, right: `${(product.length-index-1)*380+15}px` }}
-        >
-          <FontAwesomeIcon icon={faStar} />
-        </div>
-        <img
+  // console.log('product in imgandbutton',(product.length-index-1)*350+5)
+  // console.log('let me know the index after render', index)
+  return (
+    <div>
+      <div
+        className='fav'
+        onClick={() => {
+          clickStar(item)
+        }}
+        name={item.name}
+        style={{ ...starStyle }}
+      >
+        ☆{/* <FontAwesomeIcon icon={faStar} /> */}
+      </div>
+      <div >
+        <img style={{}}
           className='under-star'
           onClick={clickProduct}
           src={style[index].photo ? style[index].photo : imgs}
@@ -45,8 +54,8 @@ function ImgButton ({ item, style, index, imgs, clickProduct, clickStar,product 
           name={item.id}
         />
       </div>
-    )
-  }
-
+    </div>
+  )
+}
 
 export default memo(ImgButton)
